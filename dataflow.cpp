10381c2880423267a7c6ec7d6f6861fd0e3c64e2
Muzammil <muzammilhussain1031@gmail.com>
2019-04-01 22:39:09 +0500

    dataflow.cpp added

--- dataflow.cpp
+++ dataflow.cpp
@@ -5,12 +5,12 @@
 #include "dataflow.h"
 
 namespace llvm {
-   iterative_model::iterative_model(bool direction, Function &p, BitVector* (*mt)(BasicBlock*, std::map<BasicBlock*, block_info*>&) ,  BitVector* (*trns)(BasicBlock*,std::map<BasicBlock*, block_info*>&),int number){
-    fwd = direction;
-    func = &p;
-    meet = mt;
-    transfer_function = trns;
-       for(Function::iterator FI=func->begin(), FE=func->end(); FI!=FE; ++FI){
+    iterative_model::iterative_model(bool direction, Function &p, BitVector* (*mt)(BasicBlock*, std::map<BasicBlock*, block_info*>&) ,  BitVector* (*trns)(BasicBlock*,std::map<BasicBlock*, block_info*>&),int number){
+        fwd = direction;
+        func = &p;
+        meet = mt;
+        transfer_function = trns;
+        for(Function::iterator FI=func->begin(), FE=func->end(); FI!=FE; ++FI){
             BasicBlock* block = &*FI;
             block_info* info = new block_info(number);
             state[block] = info;
@@ -18,30 +18,46 @@ namespace llvm {
     }
 
 
-BitVector* iterative_model::run_analysis(){
-    bool converged = false;
-    if(fwd){
-        BasicBlock* starting_point=&*func->begin();
-        BitVector* out2;
-        BitVector* out= state[starting_point]->input;
-        while(!converged){
-            for(Function::iterator FI=func->begin(), FE=func->end(); FI!=FE; ++FI){
-                converged = false;
-                BasicBlock* block = &*FI;
-                state[block]->input = meet(block,state);
-                out2 = transfer_function(block,state);
-                if(out == out2){
-                    converged = true;
+    BitVector* iterative_model::run_analysis(){
+        bool converged = false;
+        if(fwd){
+            BasicBlock* starting_point=&*func->begin();
+            BitVector* out2;
+            BitVector* out= state[starting_point]->input;
+            while(!converged){
+                for(Function::iterator FI=func->begin(), FE=func->end(); FI!=FE; ++FI){
+                    converged = false;
+                    BasicBlock* block = &*FI;
+                    state[block]->input = meet(block,state);
+                    out2 = transfer_function(block,state);
+                    if(out == out2){
+                        converged = true;
+                    }
+                    state[block]->output=out2;
                 }
-                state[block]->output=out2;
+                out = out2;
             }
-            out = out2;
+        }else{
+            BasicBlock* starting_point=&*func->getBasicBlockList().rbegin();
+            BitVector* input2;
+            BitVector* input= state[starting_point]->input;
+            while(!converged){
+                for(auto FI=func->getBasicBlockList().rbegin(), FE=func->getBasicBlockList().rend(); FI!=FE; ++FI)
+                {
+                    converged=false;
+                    BasicBlock* block = &*FI;
+                    state[block]->output = meet(block,state); 
+                    input2 = transfer_function(block,state);
+                    if(input == input2){
+                        converged = true;
+                    }
+                    state[block]->input=input2;
+                }
+            }
         }
-
-    }else{}
         BitVector* toreturn = new BitVector(1);
         return toreturn;
-   }
+    }
     // Add code for your dataflow abstraction here.
 }
 
